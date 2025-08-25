08388ec25ee537b9a13ef2fcea9b3a1c5fb59eef
lum951159 <luizmboas@gmail.com>
2025-08-25 16:53:10 -0300

    ionic chato

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage {
+  email: string = '';
+  senha: string = '';
+
+  entrar() {
+    console.log('Email:', this.email);
+    console.log('Senha:', this.senha);
+
+  }
+}
