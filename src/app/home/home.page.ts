6766ad180754c83c8edf4e7e951427996228bc47
juliawadac <juliawadac@gmail.com>
2025-09-04 15:13:21 -0300

    com o categorias scrollavel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,12 @@ export class HomePage {
 
   constructor() {}
 
+scrollTo(id: string) {
+  const el = document.getElementById(id);
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
+
 }
 
