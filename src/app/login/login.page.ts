3404496f73752cd332db3fc7c769bc98b1bcf4ff
Luiz Boas <luizmboas@gmail.com>
2025-09-03 23:32:48 -0300

    arrumei todas as rotas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, FormsModule], // ngModel e componentes Ionic
+  imports: [CommonModule, FormsModule, IonicModule, HttpClientModule, RouterModule],
 })
 export class LoginPage {
   email = '';
@@ -19,17 +19,13 @@ export class LoginPage {
   constructor(private http: HttpClient, private router: Router) { }
 
   login() {
-    const dadosDoLogin = {
-      email: this.email,
-      senha: this.senha
-    };
+    const dadosDoLogin = { email: this.email, senha: this.senha };
 
     this.http.post('http://localhost:3000/usuarios/login', dadosDoLogin)
       .subscribe({
         next: (res: any) => {
-          // se a API retornar token:
           if (res?.token) localStorage.setItem('token', res.token);
-          this.router.navigateByUrl('/home'); // ajuste a rota destino se necessário
+          this.router.navigateByUrl('/home'); // Agora funciona!
         },
         error: (err) => {
           console.error('Erro no login:', err);
